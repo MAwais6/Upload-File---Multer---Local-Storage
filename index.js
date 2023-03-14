e426af7b2625e020914cabe9577a0edbecaf2448
MAwais6 <f200265@cfd.nu.edu.pk>
2023-03-14 06:18:51 +0500

    Upload Audio & Video and Images Successfully

--- index.js
+++ index.js
@@ -10,36 +10,39 @@ app.get('/', (req, res) => {
     }
 );
 
-
-app.post('/upload', (req, res) => {
-    const storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, 'uploads/')
-        },
-        filename: function (req, file, cb) {
-            cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-        }
-    });
-    const upload = multer({ 
-        storage: storage,
-        fileFilter: function (req, file, cb) {
-            if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-                cb(null, true)
-            } else {
-                cb(null, false)
-                return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
-            }
-        }
-    }).single('user_image');
-    upload(req, res, function (err) {
-        if (err instanceof multer.MulterError) {
-            return res.status(500).json(err)
-        } else if (err) {
-            return res.status(500).json(err)
-        }
-        return res.status(200).send(req.file)
-    })
-});
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      let folder;
+      if (file.mimetype.startsWith('image/')) {
+        folder = 'uploads/images/';
+      } else if (file.mimetype.startsWith('audio/')) {
+        folder = 'uploads/audios/';
+      } else if (file.mimetype.startsWith('video/')) {
+        folder = 'uploads/videos/';
+      } else {
+        return cb(new Error('File type not supported'));
+      }
+      cb(null, folder);
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+  });
+  
+  const upload = multer({
+    storage: storage,
+    fileFilter: function(req, file, cb) {
+      const allowedTypes = ['image/jpeg', 'image/png', 'audio/mp3', 'video/mp4' , 'video/quicktime' , "audio/mpeg"];
+      if (!allowedTypes.includes(file.mimetype)) {
+        return cb(new Error('Only JPEG, PNG, MP3, and MP4 files are allowed'));
+      }
+      cb(null, true);
+    }
+  });
+  
+  app.post('/upload', upload.single('file'), (req, res) => {
+    res.send('File uploaded successfully');
+  });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
